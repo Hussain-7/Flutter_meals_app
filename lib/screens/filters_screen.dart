9f379c6f24019e99def76e85a35a308170862eed
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2022-04-27 10:14:10 +0500

    routes replacement used instead of pushing everytime

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   static const route = "/filers";
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Filters Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Filters'),
+      ),
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text('Filters Screen'),
+      ),
     );
   }
 }
